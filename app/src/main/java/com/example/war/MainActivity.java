73b311abe3257758e9139b0401992c83459f1580
Kharlenkow <yhlzlj123456@163.com>
2020-07-22 16:52:24 +0800

    Merge s1

--- app/src/main/java/com/example/war/MainActivity.java
+++ app/src/main/java/com/example/war/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.war;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,11 +13,25 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Vector;
 
 
@@ -21,7 +39,8 @@ public class MainActivity extends AppCompatActivity {
 
     private Button CButton;
     private Switch State;
-    private TextView Port,CMDLine;
+    private TextView Port,CMDLine,PresentData;
+    private LineChart Chart;
 
     private int port;
 
@@ -30,65 +49,67 @@ public class MainActivity extends AppCompatActivity {
 
     public int DataNow;
     public Vector DataArray;
+    private LineDataSet linedataset;
+    private LineData linedata;
 
-    /*private boolean IpCheck(TextView ipcontainer)
-    {
-        String cache=ipcontainer.getText().toString();
-        int ip1=0,ip2=0,ip3=0,ip4=0;
-        int count=0;
-        for(int i=0;i<cache.length();i++)
-        {
-            if(cache.charAt(i)=='.')
-            {
-                count++;
-            }
-            else {
-                switch (count) {
-                    case 0: {
-                        ip1 *= 10;
-                        ip1 += (int)cache.charAt(i) -(int) '0';
-                        break;
-                    }
-                    case 1: {
-                        ip2 *= 10;
-                        ip2 += (int)cache.charAt(i) - (int)'0';
-                        break;
-                    }
-                    case 2: {
-                        ip3 *= 10;
-                        ip3 += (int)cache.charAt(i) - (int)'0';
-                        break;
-                    }
-                    case 3: {
-                        ip4 *= 10;
-                        ip4 += (int)cache.charAt(i) -(int) '0';
-                        break;
-                    }
-                    default:
-                        return false;
-                }
-            }
-        }
-        if(count!=3)
-            return false;
-        if(ip1>255||ip2>255||ip3>255||ip4>255)
-            return false;
-        ipaddr=cache;
-        return true;
-    }
-    */
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+
+
         CButton=(Button)findViewById(R.id.Confirm);
         Port=(TextView)findViewById(R.id.Port);
         State=(Switch)findViewById(R.id.ConnectionState);
         CMDLine=(TextView)findViewById(R.id.CMDLine);
+        CMDLine.setKeyListener(null);
+        PresentData=(TextView)findViewById(R.id.PresentData);
+        PresentData.setKeyListener(null);
+        Chart=(LineChart)findViewById(R.id.Chart);
 
         DataNow=0;
+        DataArray=new Vector();
+
+        Chart.setNoDataText("Data Real Time");
+        Chart.setTouchEnabled(true);
+        Chart.setBackgroundColor(Color.WHITE);
+        Chart.setScaleEnabled(true);
+        Chart.setDrawGridBackground(true);
+        Chart.setVisibleXRangeMaximum(20);
+
+        linedataset=new LineDataSet(null,"Frequency");
+        linedataset.setColor(Color.CYAN);
+        linedataset.setCircleColor(Color.GREEN);
+        //linedataset.setCircleSize();
+        linedataset.setLineWidth(3f);
+        linedataset.setDrawValues(true);
+        linedataset.setAxisDependency(YAxis.AxisDependency.LEFT);
+
+        linedata=new LineData(linedataset);
+        linedata.setValueTextColor(Color.BLUE);
+        Chart.setData(linedata);
+        Legend Flegend=Chart.getLegend();
+        Flegend.setForm(Legend.LegendForm.CIRCLE);
+        Flegend.setTextColor(Color.BLUE);
+
+        XAxis xl = Chart.getXAxis();
+        xl.setTextColor(Color.BLUE);
+        xl.setDrawGridLines(true);
+        xl.setAvoidFirstLastClipping(true);
+        xl.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xl.setAxisMinimum(0f);
+
+        YAxis yl = Chart.getAxisLeft();
+        yl.setTextColor(Color.BLUE);
+        yl.setDrawGridLines(true);
+        yl.setAxisMinimum(0f);
+
+        YAxis rightAxis = Chart.getAxisRight();
+        rightAxis.setEnabled(false);
+
 
         CButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -124,38 +145,63 @@ public class MainActivity extends AppCompatActivity {
                                         State.setChecked(true);
                                     }
                                 });
-                                BufferedReader Bin=new BufferedReader(new InputStreamReader(Client_Socket.getInputStream()));
+                                BufferedReader Bin=new BufferedReader(new InputStreamReader(Client_Socket.getInputStream(),"UTF-8"));
                                 while (true) {
                                     final String Cache = Bin.readLine();
-                                    /*if (cache.isEmpty()) {
-                                        Bin.close();
-                                        Client_Socket.close();
-                                        runOnUiThread(new Runnable() {
-                                            @Override
-                                            public void run() {
-                                                CButton.setClickable(true);
-                                                State.setChecked(false);
-                                            }
-                                        });
-                                        return;
-                                    }*/
                                     if(Cache!=null) {
                                         if (!Cache.isEmpty()) {
                                             DataNow = Integer.parseInt(Cache);
-                                            //DataArray.addElement(DataNow);
+                                            //if(DataArray==null)
+                                            {
+                                                //DataArray.add(new Entry(0,DataNow));
+                                                //linedataset.addEntry(new Entry(0,DataNow));
+                                            }
+                                            //else {
+                                                //DataArray.add(new Entry(DataArray.size(), DataNow));
+                                            //}
+                                            //linedataset.addEntry(new Entry(linedataset.getXMax(),DataNow));
+                                            DataArray.addElement(DataNow);
+                                            linedataset.addEntry(new Entry(linedataset.getEntryCount(),DataNow));
+                                            linedata.notifyDataChanged();
+                                            Chart.notifyDataSetChanged();
+
+
+
                                             runOnUiThread(new Runnable() {
                                                 @Override
                                                 public void run() {
-                                                    CMDLine.append(Cache);
-                                                    CMDLine.append("\n");
+                                                    try {
+                                                        PresentData.setText(Cache);
+                                                        CMDLine.append(Cache);
+                                                        CMDLine.append("\n");
+                                                        Chart.moveViewToX(linedataset.getEntryCount()-20);
+                                                        Toast.makeText(getApplicationContext(),Integer.toString(linedataset.getEntryCount()-20),Toast.LENGTH_LONG).show();
+                                                        //Chart.invalidate();
+                                                        //Chart.notifyDataSetChanged();
+                                                        //Chart.invalidate();
+                                                    }
+                                                    catch(Exception E)
+                                                    {
+                                                        Toast.makeText(getApplicationContext(),E.getMessage(),Toast.LENGTH_LONG).show();
+                                                    }
                                                 }
                                             });
                                         }
                                     }
+
                                 }
                             }
                             catch(final Exception ex)
                             {
+                                try {
+                                    Client_Socket.close();
+                                    Server_Socket.close();
+                                }
+                                catch (Exception e)
+                                {
+                                    Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+                                }
+
                                 runOnUiThread(new Runnable() {
                                     @Override
                                     public void run() {
